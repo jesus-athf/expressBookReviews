516cd75a574ad1a339e55861e62e37842445b977
jesus_athf <michischaetzle@web.de>
2023-09-27 12:09:55 -0400

    final changes incl. task 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -63,4 +63,58 @@ public_users.get('/review/:isbn',function (req, res) {
     res.send(books[isbnRev]["reviews"]);
 });
 
+// TASK 10 - Get the book list available in the shop using promises
+public_users.get('/allbooks',function (req, res) {
+
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+      });
+
+      get_books.then(() => console.log("Promise for Task 10 resolved"));
+
+  });
+
+// TASK 11 - Get the book details based on ISBN using promises
+public_users.get('/books/isbn/:isbn',function (req, res) {
+    const get_books_isbn = new Promise((resolve, reject) => {
+
+    let booksbyisbn = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+        if(books[isbn]["isbn"] === req.params.isbn) {
+            booksbyisbn.push({"isbn":isbn,
+                              "title":books[isbn]["title"],
+                              "author":books[isbn]["author"]});
+        resolve(res.send(JSON.stringify({booksbyisbn}, null, 4)));
+        }
+    });
+    reject(res.send("This isbn does not exist"))
+    })
+
+// TASK 12 - Get book details based on author
+public_users.get('/books/author/:author',function (req, res) {
+
+    const get_books_author = new Promise((resolve, reject) => {
+
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      resolve(res.send(JSON.stringify({booksbyauthor}, null, 4)));
+      }
+    });
+    reject(res.send("The mentioned author does not exist ")) 
+    });
+
+    get_books_author.then(function(){
+            console.log("Promise is resolved");
+   }).catch(function () { 
+                console.log('The mentioned author does not exist');
+  });
+
+  });
+
 module.exports.general = public_users;
